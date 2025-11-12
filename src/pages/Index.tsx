9dcc0666548ae343569b893532140df20cb238e0
MBDEV <temploluxury@gmail.com>
2025-11-11 20:38:24 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -142,8 +142,8 @@ const Index = () => {
               <div className="flex flex-col md:flex-row gap-4 w-full max-w-2xl justify-center">
 
                 {/* Video 1 - Formato vertical 9:16 */}
-                <Card className="overflow-hidden shadow-lg flex flex-col w-full max-w-[180px] mx-auto">
-                  <div className="h-80 aspect-[9/16]"> {/* Formato 9:16 exacto */}
+                <Card className="overflow-hidden shadow-lg flex flex-col w-full max-w-[250px] mx-auto">
+                  <div className="h-64 aspect-[9/16]"> {/* Formato 9:16 exacto */}
                     <video
                       className="w-full h-full object-cover"
                       autoPlay
@@ -164,8 +164,8 @@ const Index = () => {
                 </Card>
 
                 {/* Video 2 - Formato vertical 9:16 */}
-                <Card className="overflow-hidden shadow-lg flex flex-col w-full max-w-[180px] mx-auto">
-                  <div className="h-80 aspect-[9/16]"> {/* Formato 9:16 exacto */}
+                <Card className="overflow-hidden shadow-lg flex flex-col w-full max-w-[250px] mx-auto">
+                  <div className="h-64 aspect-[9/16]"> {/* Formato 9:16 exacto */}
                     <video
                       className="w-full h-full object-cover"
                       autoPlay
